79c1f3dd2b97c043a4dfd34cac7b61e6d9a5d731
David Oren <david17895@gmail.com>
2021-02-11 13:34:34 +0200

    Footer disclaimer

--- client/src/Common/Footer.js
+++ client/src/Common/Footer.js
@@ -3,13 +3,26 @@ import logo from '../images/logo.png';
 import styled from 'styled-components';
 import { colorsVariables } from '../GlobalStyles';
 import { Link } from 'react-router-dom';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <OutterFooter>
       <StyledFooter>
-        {' '}
-        <Link to='/'>
+        <Disclaimer>
+          <p>
+            Disclaimer: All the products in this website are fictional. No
+            payments are real so feel free to navigate and place "orders"
+          </p>
+        </Disclaimer>
+        <Links>
+          <Link to=''>
+            <p>
+              Github: <FaGithub />
+            </p>
+          </Link>
+        </Links>
+        <Link to='/https://github.com/knowledgeReignsSupreme'>
           <img src={logo} alt='logo' />
         </Link>
       </StyledFooter>
@@ -19,10 +32,11 @@ const Footer = () => {
 
 const OutterFooter = styled.footer`
   width: 100%;
-  height: 4rem;
+  height: auto;
   z-index: 3;
   background: ${colorsVariables.colorSecDark};
   margin-top: 1rem;
+  padding: 1rem 0;
 `;
 
 const StyledFooter = styled.div`
@@ -31,9 +45,31 @@ const StyledFooter = styled.div`
   margin: 0 auto;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+
+  p {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    text-align: center;
+  }
+
   img {
     width: 4rem;
     margin: 0 auto;
+  }
+`;
+
+const Disclaimer = styled.div``;
+
+const Links = styled.div`
+  cursor: pointer;
+
+  p,
+  svg {
+    font-size: 1.3rem !important;
+    color: ${colorsVariables.colorMainDark};
   }
 `;
 
