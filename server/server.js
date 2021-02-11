0997f517364a4629fe6e75179ea85551029cecad
David Oren <david17895@gmail.com>
2021-02-11 10:16:37 +0200

    Improve server security

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -9,6 +15,12 @@ const errorHandler = require('./middleware/error');
 
 dotenv.config();
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 10000,
+  max: 100,
+  message: 'Too many requests to the server, please try again later',
+});
+
 //Basic init
 const PORT = process.env.PORT || 5800;
 
@@ -17,6 +29,13 @@ connectDB();
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(cookieParser());
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
